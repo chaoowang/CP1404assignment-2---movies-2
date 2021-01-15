9f24d3b19e040490c8a54d74f968480968de71f1
wangchaohsuan <chaohsuan.wang@my.jcu.edu.au>
2021-01-16 01:37:51 +0800

    add class Movie

--- movie.py
+++ movie.py
@@ -1,9 +1,16 @@
-"""..."""
-
-
-# TODO: Create your Movie class in this file
+"""Movie class"""
 
 
 class Movie:
-    """..."""
-    pass
+    """represent a Movie object"""
+
+    def __init__(self, title="", year=0, category="", is_watched=False):
+        """initialise a Movie"""
+        self.title = title
+        self.year = year
+        self.category = category
+        self.is_watched = is_watched
+
+    def __str__(self):
+        """return a string representative of a Movie"""
+        return "{}, {}, {}, {}".format(self.title, self.year, self.category, self.is_watched)
