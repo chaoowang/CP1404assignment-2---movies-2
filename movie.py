bd9d6e509c5fe8b6308cc3f205bab6e0757ae0fd
wangchaohsuan <chaohsuan.wang@my.jcu.edu.au>
2021-01-22 03:02:42 +0800

    add comment

--- movie.py
+++ movie.py
@@ -16,9 +16,11 @@ class Movie:
         return "{}, {}, {}, {}".format(self.title, self.year, self.category, self.is_watched)
 
     def watch(self):
+        """mark movie as watched"""
         self.is_watched = "w"
         return self.is_watched
 
     def unwatch(self):
+        """mark movie as unwatch"""
         self.is_watched = "u"
         return self.is_watched
